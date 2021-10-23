3e6ab4702e8b363cf301855d4e69ae741533339e
Tamas Szabo <murvai.rebeka@gmail.com>
2021-10-23 15:50:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args){
+        //1
+        System.out.println("Hello \n");
+        System.out.println("Murvai Rebeka");
+        //2
+        int a=2;
+        int b=996;
+        int c=a+b;
+        System.out.println("2+996=" + c);
+        //3
+        double d= 10.0 / 3.0;
+        System.out.println(d);
+        //4
+        System.out.println(-5 + 8 * 6);
+        System.out.println( (55+9) % 9 );
+        System.out.println( (55+9) /9 );
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+
+
+    }
+}
